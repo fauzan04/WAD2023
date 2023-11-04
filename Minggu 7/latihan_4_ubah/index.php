8fd1f4a0483b0398d57206a4a27024a6b7a856ba
Fauzan <fauzanvictor04@gmail.com>
2023-11-05 00:17:33 +0700

    commit

---
+++ Minggu 7/latihan_4_ubah/index.php
@@ -0,0 +1,92 @@
+<?php
+require 'functions.php';
+$brg = query("SELECT * FROM toko_buah_aliong");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HALAMAN ADMIN</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+        }
+
+        h1 {
+            text-align: center;
+            color: #ff6633;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 80%;
+            margin: 20px auto;
+            border: 1px solid #ddd;
+            background-color: #fff;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 10px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #ffcc66;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        a {
+            text-decoration: none;
+            margin-right: 10px;
+            padding: 5px 10px;
+            background-color: #ff6633;
+            color: #fff;
+            border: 1px solid #ff6633;
+            border-radius: 4px;
+        }
+
+        a:hover {
+            background-color: #ff3300;
+            border: 1px solid #ff3300;
+        }
+
+        #tombol-tambah {
+            text-align: center;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1>DAFTAR BUAH TOKO ALIONG</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Kode Buah</th>
+            <th>Nama Buah</th>
+            <th>Harga Buah</th>
+            <th>Kadaluarsa</th>
+        </tr>
+        <?php foreach ($brg as $index => $barang) : ?>
+            <tr>
+                <td><?= $index + 1 ?></td>
+                <td>
+                    <a href="ubah.php?kode_buah=<?= $barang['kode_buah'] ?>">Ubah</a>
+                    <a href="">Hapus</a>
+                </td>
+                <td><?= $barang['kode_buah'] ?></td>
+                <td><?= $barang['nama_buah'] ?></td>
+                <td><?= $barang['harga'] ?></td>
+                <td><?= $barang['tanggal_kadaluarsa'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
---
+++ Minggu 7/latihan_4_ubah/ubah.php
@@ -0,0 +1,118 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "latihanwad");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_GET["kode_buah"])) {
+    $kode_buah = $_GET["kode_buah"];
+    $query = "SELECT * FROM toko_buah_aliong WHERE kode_buah = '$kode_buah'";
+    $result = mysqli_query($conn, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $barang = mysqli_fetch_assoc($result);
+    } else {
+        die("Barang tidak ditemukan.");
+    }
+}
+
+if (isset($_POST["submit"])) {
+    $kode_buah = $_POST["kode_buah"];
+    $nama_buah = $_POST["nama_buah"];
+    $harga = $_POST["harga"];
+    $tanggal_kadaluarsa = $_POST["tanggal_kadaluarsa"];
+
+    $query = "UPDATE toko_buah_aliong SET nama_buah = '$nama_buah', harga = '$harga', tanggal_kadaluarsa = '$tanggal_kadaluarsa' WHERE kode_buah = '$kode_buah'";
+
+    if (mysqli_query($conn, $query)) {
+        echo "Data berhasil diubah.";
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($conn);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubah data toko buah aliong</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+        }
+
+        h1 {
+            text-align: center;
+            color: #ff6633;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+        }
+
+        ul {
+            list-style: none;
+            padding: 0;
+        }
+
+        li {
+            margin-bottom: 10px;
+        }
+
+        label {
+            display: block;
+            color: #333;
+        }
+
+        input[type="text"] {
+            width: 100%;
+            padding: 5px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+        }
+
+        button[type="submit"] {
+            background-color: #ff6633;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button[type="submit"]:hover {
+            background-color: #ff3300;
+        }
+    </style>
+</head>
+<body>
+    <h1>Ubah data toko buah aliong</h1>
+    <form action="ubah.php?kode_buah=<?= $barang['kode_buah'] ?>" method="post">
+        <input type="hidden" name="kode_buah" value="<?= $barang['kode_buah'] ?>">
+        <ul>
+            <li>
+                <label for="nama_buah">Nama Buah : </label>
+                <input type="text" name="nama_buah" id="nama_buah" value="<?= $barang['nama_buah'] ?>">
+            </li>
+            <li>
+                <label for="harga">Harga Buah : </label>
+                <input type="text" name="harga" id="harga" value="<?= $barang['harga'] ?>">
+            </li>
+            <li>
+                <label for="tanggal_kadaluarsa">Tanggal Kadaluarsa : </label>
+                <input type="text" name="tanggal_kadaluarsa" id="tanggal_kadaluarsa" value="<?= $barang['tanggal_kadaluarsa'] ?>">
+            </li>
+        </ul>
+        <button type="submit" name="submit">Ubah Data</button>
+    </form>
+</body>
+</html>
